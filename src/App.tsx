25b9f44f51b90a5f825eb192210f38e550975711
Hat52 <tanolihamzaali@gmail.com>
2023-08-27 15:31:18 +0500

    cards created

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,15 @@ import './App.css';
 
 function App() {
 	return (
-		<div className="">
-			<div className="bg-[#F3F0F1] w-[20%] h-[200px] shadow-lg">asd</div>
+		<div className="px-40 py-10">
+			<div className="grid grid-cols-4 gap-4">
+				<div className="bg-[#F3F0F1] w-full h-[200px] shadow-lg">asd</div>
+				<div className="bg-[#F3F0F1] w-full h-[200px] shadow-lg">asd</div>
+				<div className="bg-[#F3F0F1] w-full h-[200px] shadow-lg">asd</div>
+				<div className="bg-[#F3F0F1] w-full h-[200px] shadow-lg">asd</div>
+				<div className="bg-[#F3F0F1] w-full h-[200px] shadow-lg">asd</div>
+				<div className="bg-[#F3F0F1] w-full h-[200px] shadow-lg">asd</div>
+			</div>
 		</div>
 	);
 }
