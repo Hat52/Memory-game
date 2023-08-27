aff06482824d3516887cf0a22fc2c99f503e4353
Hat52 <tanolihamzaali@gmail.com>
2023-08-28 01:46:20 +0500

    Card rotation animation

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,22 @@ import './App.css';
 
 function App() {
 	return (
-		<div className="px-40 py-10">
-			<div className="grid grid-cols-4 gap-4">
-				<div className="bg-[#F3F0F1] w-full h-[200px] shadow-lg">asd</div>
-				<div className="bg-[#F3F0F1] w-full h-[200px] shadow-lg">asd</div>
-				<div className="bg-[#F3F0F1] w-full h-[200px] shadow-lg">asd</div>
-				<div className="bg-[#F3F0F1] w-full h-[200px] shadow-lg">asd</div>
-				<div className="bg-[#F3F0F1] w-full h-[200px] shadow-lg">asd</div>
-				<div className="bg-[#F3F0F1] w-full h-[200px] shadow-lg">asd</div>
+		<div className="md:px-40 px-5 py-10">
+			<div className="grid grid-cols-1 md:grid-cols-4 gap-4">
+				{Array(12)
+					.fill(0)
+					.map(() => (
+						<div className="flip-card">
+							<div className="flip-card-inner">
+								<div className="flip-card-front"></div>
+								<div
+									// className={`w-full h-[200px] transform duration-300  hover:rotate-180 shadow-lg bg-[#DFDBE5] rounded-lg`}
+									className="flip-card-back">
+									asd
+								</div>
+							</div>
+						</div>
+					))}
 			</div>
 		</div>
 	);
