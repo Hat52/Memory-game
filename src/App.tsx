eee3d53d244e6b78e20148c68b5da63762b412ab
Hat52 <tanolihamzaali@gmail.com>
2023-08-28 02:21:48 +0500

    flip animation added

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,25 @@
 import './App.css';
-import Background from './assets/card.svg';
+import { gameData } from './constants/data';
 
 function App() {
 	return (
 		<div className="md:px-40 px-5 py-10">
 			<div className="grid grid-cols-1 md:grid-cols-4 gap-4">
-				{Array(12)
-					.fill(0)
-					.map(() => (
-						<div className="flip-card rounded-lg overflow-hidden">
-							<div className="flip-card-inner">
-								<div className="flip-card-front" style={{ background: Background }}></div>
-								<div className="flip-card-back">asd</div>
+				{gameData.map((d: any) => (
+					<div className="flip-card rounded-lg overflow-hidden">
+						<div className="flip-card-inner">
+							<div className="flip-card-front"></div>
+							<div
+								className="flip-card-back"
+								style={{
+									backgroundColor: 'black !important',
+									backgroundImage: 'url("./assets/Dog-With-Blue-Eyes.svg") !important'
+								}}>
+								asdef
 							</div>
 						</div>
-					))}
+					</div>
+				))}
 			</div>
 		</div>
 	);
---
+++ src/constants/data.ts
@@ -0,0 +1,52 @@
+import scobyDoo from '../assets/scooby-doo.svg';
+
+export const gameData = [
+	{
+		url: scobyDoo,
+		name: ''
+	},
+	{
+		url: './assets/scooby-doo.svg',
+		name: ''
+	},
+	{
+		url: './assets/scooby-doo.svg',
+		name: ''
+	},
+	{
+		url: './assets/scooby-doo.svg',
+		name: ''
+	},
+	{
+		url: './assets/scooby-doo.svg',
+		name: ''
+	},
+	{
+		url: './assets/scooby-doo.svg',
+		name: ''
+	},
+	{
+		url: './assets/scooby-doo.svg',
+		name: ''
+	},
+	{
+		url: './assets/scooby-doo.svg',
+		name: ''
+	},
+	{
+		url: './assets/scooby-doo.svg',
+		name: ''
+	},
+	{
+		url: './assets/scooby-doo.svg',
+		name: ''
+	},
+	{
+		url: './assets/scooby-doo.svg',
+		name: ''
+	},
+	{
+		url: './assets/scooby-doo.svg',
+		name: ''
+	}
+];
