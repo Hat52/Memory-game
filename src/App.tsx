045c3d319764e0dc4ffaa4b4cf11519990a11315
Hat52 <tanolihamzaali@gmail.com>
2023-08-27 15:28:51 +0500

    configured tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import './App.css';
 
 function App() {
-	return <>Memory gameplay</>;
+	return (
+		<div className="">
+			<div className="bg-[#F3F0F1] w-[20%] h-[200px] shadow-lg">asd</div>
+		</div>
+	);
 }
 
 export default App;
