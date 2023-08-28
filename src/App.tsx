838e4417265d7ae50a4d7e8ef2467bc172666edd
Hat52 <tanolihamzaali@gmail.com>
2023-08-28 15:34:09 +0500

    flip card on click

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,35 @@
+import { useState } from 'react';
 import './App.css';
 import { gameData } from './constants/data';
+
+type TCurrentFlip = {
+	[key: number]: boolean;
+};
 
 function App() {
 	gameData[0].fliped = true;
 	gameData[5].matched = true;
+
+	const [currentFlip, setCurrentFlip] = useState<TCurrentFlip>({});
+
+	const handleClick = (index: number) => {
+		if (Object.keys(currentFlip).length >= 2) {
+			setCurrentFlip({ [index]: true });
+		} else {
+			setCurrentFlip((prev) => ({ ...prev, [index]: true }));
+		}
+	};
+
 	return (
 		<div className="md:px-60 px-5 py-10">
 			<div className="grid grid-cols-1 md:grid-cols-4 gap-4">
-				{gameData.map((d: any) => (
+				{gameData.map((d: any, index: number) => (
 					<div
+						onClick={() => handleClick(index)}
 						className={`flip-card rounded-lg overflow-hidden ${
-							d.fliped || d.matched ? 'rotate' : ''
+							Object.prototype.hasOwnProperty.call(currentFlip, index) || d.matched
+								? 'rotate'
+								: ''
 						}`}>
 						<div className="flip-card-inner">
 							<div className="flip-card-front"></div>
