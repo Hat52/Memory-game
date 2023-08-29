b56c6ca682cdcdb3031fffe4297f35d14dcf2f58
Hat52 <tanolihamzaali@gmail.com>
2023-08-29 14:00:29 +0500

    feat: score card design

--- src/App.tsx
+++ src/App.tsx
@@ -25,27 +25,52 @@ function App() {
 	}, []);
 
 	return (
-		<div className="md:px-60 px-5 py-10">
-			<div className="grid grid-cols-1 md:grid-cols-4 gap-4">
-				{gameData.map((d: any, index: number) => (
-					<div
-						onClick={() => handleClick(index)}
-						className={`flip-card 2xl:h-[250px] rounded-lg overflow-hidden ${
-							currentFlips.some((flip: number) => flip === index) || d.matched
-								? 'rotate'
-								: ''
-						}`}>
-						<div className="flip-card-inner">
-							<div className="flip-card-front"></div>
-							<div className="flip-card-back">
-								<img src={d.url} alt={d.name} className="w-full h-full object-cover" />
+		<div>
+			<div className="sidebar absolute top-0 bottom-0 w-[10%] justify-center shadow-[#6DF6F2] bg-[#6DF6F260] shadow-lg flex flex-col">
+				<Item title="Guesses" value="10" />
+				<Item title="Guesses" value="10" />
+				<Item title="Guesses" value="10" />
+				<Item title="Guesses" />
+				<button className="bg-[#57D0C3] mx-5 p-2 text-white font-bold mt-10 rounded-md">
+					RESET
+				</button>
+			</div>
+			<div className="md:px-60 px-5 py-10 relative">
+				<div className="grid grid-cols-1 md:grid-cols-4 gap-4">
+					{gameData.map((d: any, index: number) => (
+						<div
+							onClick={() => handleClick(index)}
+							className={`flip-card 2xl:h-[250px] rounded-lg overflow-hidden ${
+								currentFlips.some((flip: number) => flip === index) || d.matched
+									? 'rotate'
+									: ''
+							}`}>
+							<div className="flip-card-inner">
+								<div className="flip-card-front"></div>
+								<div className="flip-card-back">
+									<img src={d.url} alt={d.name} className="w-full h-full object-cover" />
+								</div>
 							</div>
 						</div>
-					</div>
-				))}
+					))}
+				</div>
 			</div>
 		</div>
 	);
 }
+
+type TItem = {
+	title: string;
+	value?: string | number | boolean;
+};
+
+const Item = ({ title, value }: TItem) => {
+	return (
+		<div className="flex items-center my-5 flex-col gap-1">
+			<h3 className="font-bold uppercase text-[18px]">{title}</h3>
+			<p className="text-medium">{value}</p>
+		</div>
+	);
+};
 
 export default App;
